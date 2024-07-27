99daf1a3016f17f9d21362adeb1c36c9aab1fbd7
Serhii Trush <raznie.podelki@gmail.com>
2024-07-27 12:58:13 +0300

    Change#001
    Code refactoring

--- code/main.py
+++ code/main.py
@@ -4,63 +4,83 @@ import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.playback import play
 import re
+import pygame
+import time
 
-Device = "cuda:0"
-user_prompt = "Ви шанобливий помічник, розмовляєте українською мовою, коротко та лаконічно. Привітайтесь та запитайте як зовуть співрозмовника."
-#prompt_template = "<｜begin▁of▁sentence｜>User: {user_message_1}\n\nAssistant: {assistant_message_1}<｜end▁of▁sentence｜>User: {user_message_2}\n\nAssistant:\n"
+deviceProcessing = "cuda:0"
+userPrompt = "Ви розмовляєте українською мовою, в міру лаконічний. Привітайтесь та запитайте як зовуть співрозмовника."
+#systemPrompt = '<|user|>\\n{userPrompt}<|end|>\\n<|assistant|>'
+filePathMP3 = "C:\\Projects\\VoiceAssistant\\code\\temp_audio\\temp.mp3"
 
 model = Llama(
-    model_path="D:\\lm-studio\\lmstudio-community\\Meta-Llama-3-8B-Instruct-GGUF\\Meta-Llama-3-8B-Instruct-Q4_K_M.gguf",
+    model_path="D:\\lm-studio\\lmstudio-community\\Meta-Llama-3.1-8B-Instruct-GGUF\\Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf",
     chat_format="llama-3",
-    n_gpu_layers=13,
+    n_gpu_layers=6,
     flash_attn=True,
     n_threads=8,
     n_ctx=8192,
-    device=Device,
-    verbose=False) # verbose=False - debug output off
+    device=deviceProcessing,
+    verbose=True) # verbose=False - debug output off
 
-# Simple inference example
-#output = model(prompt_template, # Prompt
-#    stop=["<｜end▁of▁sentence｜>"],   # Example stop token - not necessarily correct for this specific model! Please check before using.
-#  #echo=True        # Whether to echo the prompt
-#)
 
-FilePathMP3 = "C:\\Projects\\vscode-basics\\GoIT-Python-Data-Science\\MyProjects\\VoiceAssistant\\temp.mp3"
+chatMessages=[{"role": "system", "content": userPrompt}]
 
-messages=[{"role": "system", "content": user_prompt}]
-
-def get_audio():
+def speechToText():
     r = sr.Recognizer()
-    with sr.Microphone() as source:
+    m = sr.Microphone()
+
+    with m as source: 
+        r.adjust_for_ambient_noise(source, duration=0.2) 
+    
+    with m as source:
         audio = r.listen(source)
-        said = ""
+        TextFromAudio = ""
 
         try:
-            said = r.recognize_google(audio, language="uk-UA")
-            print(f"> {said}")
+            TextFromAudio = r.recognize_google(audio, language="uk-UA")
+            print(f"> {TextFromAudio}")
+            
         except Exception as e:
             print("Скажіть шось..." + str(e))
 
-    return said
+    return TextFromAudio
 
-def play_audio(filename):
+def playAudio(filename):
     audio = AudioSegment.from_file(filename)
     octaves = 1.0
     new_sample_rate = int(audio.frame_rate * (1.25 ** octaves))
     hipitch_sound = audio._spawn(audio.raw_data, overrides={'frame_rate': new_sample_rate})
     hipitch_sound = hipitch_sound.set_frame_rate(44100)
-    play(hipitch_sound)
+    
+    #play(hipitch_sound)
+    hipitch_sound.export(filename, format="mp3")
+    
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+    
+    # Чекаємо, поки відтворення завершиться
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+    pygame.mixer.music.unload()
+    
+    #if os.path.exists(filePathMP3):
+    #    os.remove(filePathMP3)         
 
-    #if os.path.exists(FilePathMP3):
-    #    os.remove(FilePathMP3)         
+def textToSpeech(dataInput: str):
+    tts = gTTS(text=dataInput, lang='uk')
+    tts.save(filePathMP3)
 
-def AI_speack(userText: str): #-> str:
+def LLMResponce(userText: str): #-> str:
     new_message = {"role": "user", "content": userText}
-    messages.append(new_message)
+    chatMessages.append(new_message)
 
-    output = model.create_chat_completion(messages, temperature=0.5, max_tokens=1024, stream=True)
+    output = model.create_chat_completion(messages=chatMessages, 
+                                          temperature=0.5, 
+                                          max_tokens=1024, 
+                                          stream=True)
 
-    LLM_Responce = ""
+    LLMTextResponce = ""
     count = 0
 
     for chunk in output:
@@ -69,31 +89,28 @@ def AI_speack(userText: str): #-> str:
             continue
         print(delta["content"], end="", flush=True)
         
-        LLM_Responce += delta["content"]
+        LLMTextResponce += delta["content"]
+        
         pattern = r"[;:.!?]+"
-        elements = re.split(pattern, LLM_Responce)
+        elements = re.split(pattern, LLMTextResponce)
         if (len(elements)-1) > 0 and count < (len(elements)-1):
             tmp = elements[count]
             if len(tmp) > 2:
-                tts = gTTS(text=tmp, lang='uk')
-                tts.save(FilePathMP3)
-                play_audio(FilePathMP3)
+                textToSpeech(tmp)
+                playAudio(filePathMP3)
             count += 1
 
 
     print()
     
-    #tts = gTTS(text=LLM_Responce, lang='uk')
-    #tts.save(FilePathMP3)
-    #play_audio(FilePathMP3)
+    #textToSpeech(LLM_Responce)
+    #playAudio(filePathMP3)
 
-    new_message = {"role": "assistant", "content": LLM_Responce}
-    messages.append(new_message)
-
-    #print(messages)
-    #return LLM_Responce
+    new_message = {"role": "assistant", "content": LLMTextResponce}
+    chatMessages.append(new_message)
 
 while True:
-    text = get_audio()
+    text = speechToText()
+    #text = input(">> ")
     if text != "":
-        AI_speack(text)
+        LLMResponce(text)
